c5909b5eba274a86e3a455346bcf8fe0332bdc78
dsssq <598959226@qq.com>
2022-08-19 21:17:34 +0800

    更新：舞者，大舞buff期间不跳小舞，120-3SS。

--- XIVSlothCombo/Combos/PvE/DNC.cs
+++ XIVSlothCombo/Combos/PvE/DNC.cs
@@ -426,9 +426,10 @@ namespace XIVSlothCombo.Combos.PvE
                         return All.HeadGraze;
 
                     // Simple ST Standard (activates dance with no target, or when target is over HP% threshold)
+                    // dsssq:删除“ || techBurstTimer > 5”判断条件，保证大舞buff期间不跳小舞，120-3SS。
                     if ((!HasTarget() || GetTargetHPPercent() > standardStepBurstThreshold) &&
                         IsEnabled(CustomComboPreset.DNC_ST_Simple_SS) && standardStepReady &&
-                        ((!HasEffect(Buffs.TechnicalStep) && !techBurst) || techBurstTimer > 5))
+                        (!HasEffect(Buffs.TechnicalStep) && !techBurst))
                         return StandardStep;
 
                     // Simple ST Tech (activates dance with no target, or when target is over HP% threshold)
@@ -549,9 +550,10 @@ namespace XIVSlothCombo.Combos.PvE
                         return All.HeadGraze;
 
                     // Simple AoE Standard (activates dance with no target, or when target is over HP% threshold)
+                    // dsssq:删除“ || techBurstTimer > 5”判断条件，保证大舞buff期间不跳小舞，120-3SS。
                     if ((!HasTarget() || GetTargetHPPercent() > standardStepBurstThreshold) &&
                         IsEnabled(CustomComboPreset.DNC_AoE_Simple_SS) && standardStepReady &&
-                        ((!HasEffect(Buffs.TechnicalStep) && !techBurst) || techBurstTimer > 5))
+                        (!HasEffect(Buffs.TechnicalStep) && !techBurst))
                         return StandardStep;
 
                     // Simple AoE Tech (activates dance with no target, or when target is over HP% threshold)
